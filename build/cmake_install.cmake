5f658147eddc4d2e773208e3c45c9e687ccbbdd8
Aaron Nguyen <aaronn78566@gmail.com>
2024-11-12 00:49:48 -0600

    Changed folder path
    really just removed CMakeCache.txt to compile

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/boolmon/Documents/G17 Holo/Open3D/Hologram
+# Install script for directory: /home/boolmon/github/hologram_open3d
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,7 +45,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/home/boolmon/Documents/G17 Holo/Open3D/Hologram/build/install_local_manifest.txt"
+  file(WRITE "/home/boolmon/github/hologram_open3d/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -61,6 +61,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/home/boolmon/Documents/G17 Holo/Open3D/Hologram/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/home/boolmon/github/hologram_open3d/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
